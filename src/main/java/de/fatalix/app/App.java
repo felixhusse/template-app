31371981acf13da7942044b4792f43a94c9c467b
felixhusse <felix.husse@gmail.com>
2015-02-23 23:09:29 +0100

    - initial commit for template

---
+++ src/main/java/de/fatalix/app/App.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.fatalix.app;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.cdi.CDIUI;
+import org.vaadin.cdiviewmenu.ViewMenuUI;
+
+/**
+ *
+ * @author Fatalix
+ */
+@CDIUI("")
+@Theme("valo")
+public class App extends ViewMenuUI{
+    
+    
+}
